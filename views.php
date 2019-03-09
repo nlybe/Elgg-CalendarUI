8a61c7922602a0f3d878391c2f1c51be45a503d4
nlybe <nikos@lyberakis.gr>
2019-03-09 19:49:34 +0200

    fix path for moment js

--- views.php
+++ views.php
@@ -7,7 +7,7 @@
 return [
     'default' => [
         ///'calendar_ui/icon/' => __DIR__ . '/graphics',
-        'moment.js' => __DIR__ . '/vendor/moment/moment/min/moment.min.js',
+        'moment.js' => dirname(__DIR__, 2) . '/vendor/moment/moment/min/moment.min.js',
         'moment_timezone.js' => __DIR__ . '/vendors/moment/moment-timezone-with-data.js',
         'cui_datetimepicker_js.js' => __DIR__ . '/vendors/datetimepicker/jquery.datetimepicker.full.min.js',
         'cui_datetimepicker_css.css' => __DIR__ . '/vendors/datetimepicker/jquery.datetimepicker.min.css',
